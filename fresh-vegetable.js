de590a8a1c54bdff1bb27903a583799c472d28cd
MarionDuFaouet <141531424+MarionDuFaouet@users.noreply.github.com>
2023-08-23 20:39:47 +0200

    Add files via upload

--- fresh-vegetable.js
+++ fresh-vegetable.js
@@ -7,7 +7,7 @@ fresh - vegetable
 let slideIndex = 0;
 showSlides();
 
-function showSlides() 
+function showSlides() {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
@@ -19,38 +19,11 @@ function showSlides()
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
+  slides[slideIndex-1].style.display = "flex";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 4000); // Change image every 4 seconds
-
-
-
-
-/* <script temoignage>
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+  setTimeout(showSlides, 4000); // Change image every 2 seconds
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-}
-</script> */
+
+
